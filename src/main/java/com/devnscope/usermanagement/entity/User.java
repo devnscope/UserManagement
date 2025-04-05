84ee319844899545bce7f16d978ee976e044ae55
rockyranjeet <ranjeet15jan@gmail.com>
2025-04-05 13:29:20 +0530

    Refactor : added user entity

---
+++ src/main/java/com/devnscope/usermanagement/entity/User.java
@@ -0,0 +1,13 @@
+package com.devnscope.usermanagement.entity;
+
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private int userId;
+    private String username;
+    private String password;
+
+}
